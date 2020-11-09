b4c7c6052aa6f9491ba82f932a2f5cda98f9c69e
strokeaback <66413221+strokeaback@users.noreply.github.com>
2020-11-09 22:52:15 +0600

    First Commit
    Added Unity project to GitHub

---
+++ RollBall(Lab 3. Scripting)/Assets/Scripts/CubeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CubeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //void OnMove(InputValue movementValue) 
+    //{
+
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Rotate(45, 45, 45);
+        transform.Rotate(45 * Time.deltaTime, 45 * Time.deltaTime, 45 * Time.deltaTime);
+        transform.Translate(1 * Time.deltaTime, 0, 0);
+    }
+}
